331de8e5f8ee39e8853c5e613200b4a4f90d709e
tejaswiMinnu <tjminnu@github.com>
2015-10-15 23:28:07 +0530

    remove unused import

--- HotelReservation/src/Hotel_Reservation/Hotel.java
+++ HotelReservation/src/Hotel_Reservation/Hotel.java
@@ -2,7 +2,6 @@ package Hotel_Reservation;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
