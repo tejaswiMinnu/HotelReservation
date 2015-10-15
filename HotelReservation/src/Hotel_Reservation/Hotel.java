b67278a2d0524c27118953d126797df0566679c9
tejaswiMinnu <tjminnu@github.com>
2015-10-15 23:26:22 +0530

    Implementation for Hotel and reservation classes

--- HotelReservation/src/Hotel_Reservation/Hotel.java
+++ HotelReservation/src/Hotel_Reservation/Hotel.java
@@ -1,26 +1,85 @@
 package Hotel_Reservation;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Hotel {
-	private List<Room> totalRooms;
-	private List<Room> reservedRooms;
+	private List<Reservation> totalRooms;
+	private Map<String,Reservation> reservedRooms;
 	
-	
+	public Hotel(){
+		
+	}
+	public Hotel(int numberOfRooms){
+		totalRooms = new ArrayList<Reservation>();
+		reservedRooms = new HashMap<String,Reservation>();
+		for(int i=0;i<numberOfRooms;i++){
+			Reservation reservation = new Reservation(i+1,null);
+			totalRooms.add(reservation);
+		}
+	}
 	public int reserveRoom(String person){
-		return 0;
+		int freeRoomIndex = getAFreeRoom();
+		if(freeRoomIndex == -1){
+			return -1;
+		}
+		Reservation r = totalRooms.get(freeRoomIndex);
+		r.setCustomerName(person);
+		r.setReserved(true);
+		reservedRooms.put(person, r);
+		return r.getRoomNo();
 	}
 	
 	public boolean reserveRoom(String person,int roomNum){
-		return false;
+		Reservation r = getRoomByRoomNo(roomNum);
+		if(r == null){
+			return false;
+		}
+		r.setReserved(true);
+		r.setCustomerName(person);
+		reservedRooms.put(person, r);
+		return true;
 	}
 	
 	public void cancelReservations(String person){
-		
+		Reservation r = reservedRooms.get(person);
+		r.setReserved(false);
+		r.setCustomerName(null);
 	}
 	
 	public void printReservations(){
+		Set<String> personSet = reservedRooms.keySet();
+		Iterator<String> itr = personSet.iterator();
+		while(itr.hasNext()){
+			String person = itr.next();
+			System.out.println(person + "    " + reservedRooms.get(person));
+		}
+	}
+	
+	public int getAFreeRoom(){
+		for(int i=0;i<totalRooms.size();i++){
+			Reservation r = totalRooms.get(i);
+			if(!r.isReserved()){
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public Reservation getRoomByRoomNo(int roomNo){
+		for(int i=0;i<totalRooms.size();i++){
+			Reservation r = totalRooms.get(i);
+			if(r.getRoomNo()==roomNo && !r.isReserved()){
+				return r;
+			}
+		}
 		
+		return null;
 	}
 	
 }
--- HotelReservation/src/Hotel_Reservation/Reservation.java
+++ HotelReservation/src/Hotel_Reservation/Reservation.java
@@ -3,6 +3,18 @@ package Hotel_Reservation;
 public class Reservation {
 	private int roomNo;
 	private String customerName;
+	private boolean isReserved;
+	
+	public Reservation(){
+	
+	}
+	
+	public Reservation(int roomNo,String customerName){
+		this.roomNo = roomNo;
+		this.customerName = customerName;
+		this.isReserved = false;
+	}
+	
 	public int getRoomNo() {
 		return roomNo;
 	}
@@ -14,6 +26,17 @@ public class Reservation {
 	}
 	public void setCustomerName(String customerName) {
 		this.customerName = customerName;
+	}
+	public boolean isReserved() {
+		return isReserved;
+	}
+	public void setReserved(boolean isReserved) {
+		this.isReserved = isReserved;
+	}
+	@Override
+	public String toString() {
+		return "Reservation [roomNo=" + roomNo + ", customerName="
+				+ customerName + ", isReserved=" + isReserved + "]";
 	}
 	
 }
